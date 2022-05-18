dd27161251782445414d87c1b097c6c3f9c987ac
ujas-m-simformsolutions <ujas.m@simformsolutions.com>
2022-05-18 14:07:18 +0530

    :sparkles: Formatted code and updated read me

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:animated_list_demo/home.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,15 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          accentColor: Colors.green[400],
-        ),
-        home: Home(),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Color(0xffef5366)),
+      ),
+      home: Home(),
     );
   }
 }
-
-
-
